cac3cced8bd09619f6fe48a595e2037e52a13f7c
Rezwan Tariq <42382456+RezwanCAAS@users.noreply.github.com>
2026-01-25 21:21:32 -0600

    make nwk format file using markers data

---
+++ 1_phylogenetic_tree.R
@@ -0,0 +1,24 @@
+### Neighbor-joining phylogenetic tree
+
+# 1) install.packages(c("tidyverse","ape","phangorn","proxy","parallelDist"))
+library(tidyverse)
+library(ape)
+library(phangorn)
+library(proxy)
+library(parallelDist)
+
+# 2) upload the markers_data.csv file
+dos <- read_csv("markers_data.csv", col_types = cols(.default = "c")) # makers_data.csv is attached in the repository
+
+# 3) Compute NA‐aware Euclidean distance & build NJ tree
+dm_euc <- as.dist(proxy::dist(
+  doseMat,
+  method   = "Euclidean",
+  pairwise = TRUE    # ignores any NAs
+))
+# for neighbor-joining tree
+nj_euc <- NJ(dm_euc)
+nj_mid <- midpoint(nj_euc)  # midpoint‐root for symmetry
+
+# 4) write tree with support labels for MEGA/R
+write.tree(nj_mid, file = "roses_NJ_midpoint_bootstrap.nwk")
